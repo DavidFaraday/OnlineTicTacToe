d6c2991b092fa323f4edea77a1be6d049ab49c58
DavidFaraday <iosdevschool@gmail.com>
2021-07-02 20:22:49 +0300

    Initial Commit

---
+++ OnlineGameDev/OnlineGameDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGameDevApp.swift
+//  OnlineGameDev
+//
+//  Created by David Kababyan on 02/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGameDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
